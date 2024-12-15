9040ee8ae3c50cee4e4680ea0d9e94b20237b136
adwitafernanda <adwitafernanda4@gmail.com>
2024-12-15 23:59:58 +0700

    menambahkan AlamatNavigasi DestinasiInsert

--- app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,7 @@ package com.example.pertemuan10.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiInsert : AlamatNavigasi{   //object akan menjadi nama halaman/ menjadi pengenal halaman
+    override val route: String = "insert_mhs"
+}
