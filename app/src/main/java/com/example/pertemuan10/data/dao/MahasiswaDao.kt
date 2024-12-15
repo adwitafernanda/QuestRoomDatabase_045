a9e9cd36a0a2e73ca4aa605f56b7ce93d9c54c54
adwitafernanda <adwitafernanda4@gmail.com>
2024-12-15 23:27:48 +0700

    menambahkan fungsi CRUD

--- app/src/main/java/com/example/pertemuan10/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan10/data/dao/MahasiswaDao.kt
@@ -1,12 +1,32 @@
 package com.example.pertemuan10.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface MahasiswaDao {
+    //fungsi get all data
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+
+    // get Mahasiswa
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")  //mengambil data mahasiswa berdasarkan NIM
+    fun getMahasiswa(nim: String) : Flow<Mahasiswa>
+
+    //Delete Mahasiswa
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)  //menghapus data mahasiswa tertentu dari database
+
+    //Update Mahasiswa
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa) //memperbarui informasi mahasiswa yang sudah ada di database
+
     @Insert
-    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun InsertMahasiswa(mahasiswa: Mahasiswa)
 }
