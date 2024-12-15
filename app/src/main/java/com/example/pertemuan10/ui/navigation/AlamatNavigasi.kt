60bd917700e6d85493d2154db61f0a70f4db5186
adwitafernanda <adwitafernanda4@gmail.com>
2024-12-16 00:01:52 +0700

    menambahkan destinasi detail

--- app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
@@ -11,3 +11,9 @@ object DestinasiInsert : AlamatNavigasi{   //object akan menjadi nama halaman/ m
 object DestinasiHome : AlamatNavigasi{
     override val route = "home"
 }
+
+object DestinasiDetail : AlamatNavigasi{
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
