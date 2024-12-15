b05c2bf8b49414d102aa308d0707fd845b6908c1
adwitafernanda <adwitafernanda4@gmail.com>
2024-12-15 23:37:51 +0700

    menghapus data mahasiswa

--- app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
@@ -1,7 +1,13 @@
 package com.example.pertemuan10.repository
 
 import com.example.pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getAllMhs() : Flow<List<Mahasiswa>> //methode untuk memanggil fungsi untuk mendapatkan semua data
+
+    fun getMhs(nim: String) : Flow<Mahasiswa> //mengambil data mahasiswa berdasarkan Nim
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa) // menghapus data mahasiswa
 }
